8485d57f26ac14b0b5243c2532e513435222bfcf
ErbolTokpaev <erbol.tokpaev@gmail.com>
2022-04-12 16:34:47 +0600

    remove comments

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,35 +1,14 @@
 import Vue from "vue";
 import Vuex, { StoreOptions } from "vuex";
-// import { IProduct } from "@/types/IProduct";
 import { RootState } from "@/store/types";
 import { product } from "@/store/product";
 
 Vue.use(Vuex);
 
 const store: StoreOptions<RootState> = {
-  state: {
-    helloMessage: "Helaasdf",
-  },
   modules: {
     product,
   },
 };
 
 export default new Vuex.Store<RootState>(store);
-// export default new Vuex.Store({
-//   state: {
-//     products: IProducts;
-//   },
-//   getters: {},
-//   mutations: {
-//     ADD_PRODUCT(state, product: IProduct) {
-//       state.products.push(product);
-//     },
-//   },
-//   actions: {
-//     addProduct({ commit }, product: IProduct) {
-//       commit("ADD_PRODUCT", product);
-//     },
-//   },
-//   modules: {},
-// });
